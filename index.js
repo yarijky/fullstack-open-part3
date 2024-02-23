6ad8a236b19ab91aa82a2d8665df223a03063a7e
yarivigano <yari.vigano@inmagik.com>
2024-02-23 10:26:55 +0100

    exercises 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :body'))
 
 
 let phonebook = [
